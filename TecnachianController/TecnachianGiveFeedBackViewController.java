7386e5e6c3909219d9491199cd5b101f28d6230e
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 20:34:46 +0600

    add

---
+++ TecnachianController/TecnachianGiveFeedBackViewController.java
@@ -0,0 +1,46 @@
+package com.oop.mahadi.akash__dth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class TecnachianGiveFeedBackViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea feedbacktectareafxid;
+
+    @FXML
+    private TextField subnjectfxid;
+
+    @FXML
+    void BackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeFeedbackStatuysButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert feedbacktectareafxid != null : "fx:id=\"feedbacktectareafxid\" was not injected: check your FXML file 'TecnachianGiveFeedBackView.fxml'.";
+        assert subnjectfxid != null : "fx:id=\"subnjectfxid\" was not injected: check your FXML file 'TecnachianGiveFeedBackView.fxml'.";
+
+    }
+
+}
---
+++ TecnachianController/TecnachianRatingViewController.java
@@ -0,0 +1,7 @@
+package com.oop.mahadi.akash__dth.TecnachianController;
+
+public class TecnachianRatingViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
