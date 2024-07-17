7b1a213a28da70a76ee9973501a8fad3e8ad82f4
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 21:44:44 +0600

    add

---
+++ ContentManagerControllerClass/ContentManagerAddChannelViewController.java
@@ -0,0 +1,85 @@
+package com.oop.mahadi.akash__dth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class ContentManagerAddChannelViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField SchaduleFxid;
+
+    @FXML
+    private TextField avaiblaityFxid;
+
+    @FXML
+    private TextField broadfcastcompanyFxid;
+
+    @FXML
+    private TextField countryoforiginFxid;
+
+    @FXML
+    private TextField genreFxid;
+
+    @FXML
+    private DatePicker lanchdatefxid;
+
+    @FXML
+    private TextField languageFxid;
+
+    @FXML
+    private TextField namefxid;
+
+    @FXML
+    private TextField ogoFxidBinaryImageInput;
+
+    @FXML
+    private TextField ownerfxid;
+
+    @FXML
+    private TextField resolutionFxid;
+
+    @FXML
+    private TextField socialmediahandlefxid;
+
+    @FXML
+    private TextField websideFxid;
+
+    @FXML
+    void addChannelButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert SchaduleFxid != null : "fx:id=\"SchaduleFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert avaiblaityFxid != null : "fx:id=\"avaiblaityFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert broadfcastcompanyFxid != null : "fx:id=\"broadfcastcompanyFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert countryoforiginFxid != null : "fx:id=\"countryoforiginFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert genreFxid != null : "fx:id=\"genreFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert lanchdatefxid != null : "fx:id=\"lanchdatefxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert languageFxid != null : "fx:id=\"languageFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert namefxid != null : "fx:id=\"namefxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert ogoFxidBinaryImageInput != null : "fx:id=\"ogoFxidBinaryImageInput\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert ownerfxid != null : "fx:id=\"ownerfxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert resolutionFxid != null : "fx:id=\"resolutionFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert socialmediahandlefxid != null : "fx:id=\"socialmediahandlefxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+        assert websideFxid != null : "fx:id=\"websideFxid\" was not injected: check your FXML file 'ContentManagerAddChannelView.fxml'.";
+
+    }
+
+}
