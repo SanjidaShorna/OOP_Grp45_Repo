9b0b47132939f16cf5384338ca0348e3bb57fa53
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 19:33:26 +0600

    adding file

---
+++ Tecnachian/workDone.java
@@ -0,0 +1,9 @@
+package com.oop.mahadi.akash__dth.Tecnachian;
+
+import java.time.LocalDateTime;
+
+public class workDone {
+    private int serviseId,custimercareRepId,userId;
+    LocalDateTime  WorkendTime;
+    String Details;
+}
