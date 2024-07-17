84a9cda4bdc115cbe09af81919480185fc1b25e8
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 21:00:55 +0600

    dell

--- Tecnachian/workDone.java
+++
@@ -1,9 +0,0 @@
-package com.oop.mahadi.akash__dth.Tecnachian;
-
-import java.time.LocalDateTime;
-
-public class workDone {
-    private int serviseId,custimercareRepId,userId;
-    LocalDateTime  WorkendTime;
-    String Details;
-}
