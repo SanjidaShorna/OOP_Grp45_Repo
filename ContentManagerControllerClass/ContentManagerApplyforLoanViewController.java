84576401abf9bcec4ae97b72cce2d24bffbffe6f
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 22:30:45 +0600

    add

---
+++ ContentManagerControllerClass/ContentManagerApplyforLoanViewController.java
@@ -0,0 +1,86 @@
+package com.oop.mahadi.akash__dth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ContentManagerApplyforLoanViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea applicationBodyfxid;
+
+    @FXML
+    private TextField applicationSerialnofxid;
+
+    @FXML
+    private TextField closinffxid;
+
+    @FXML
+    private DatePicker datefxid;
+
+    @FXML
+    private TextField emailfxid;
+
+    @FXML
+    private TextArea endinngfxid;
+
+    @FXML
+    private TextField namefxid;
+
+    @FXML
+    private TextField namefxid1;
+
+    @FXML
+    private TextField namefxid11;
+
+    @FXML
+    private TextField namefxid111;
+
+    @FXML
+    private TextField phonefxid;
+
+    @FXML
+    private TextField subfxid;
+
+    @FXML
+    private TextField tecnachianfxid;
+
+    @FXML
+    void BackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SeeApplicationedStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert applicationBodyfxid != null : "fx:id=\"applicationBodyfxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert applicationSerialnofxid != null : "fx:id=\"applicationSerialnofxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert closinffxid != null : "fx:id=\"closinffxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert datefxid != null : "fx:id=\"datefxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert emailfxid != null : "fx:id=\"emailfxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert endinngfxid != null : "fx:id=\"endinngfxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert namefxid != null : "fx:id=\"namefxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert namefxid1 != null : "fx:id=\"namefxid1\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert namefxid11 != null : "fx:id=\"namefxid11\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert namefxid111 != null : "fx:id=\"namefxid111\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert phonefxid != null : "fx:id=\"phonefxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert subfxid != null : "fx:id=\"subfxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+        assert tecnachianfxid != null : "fx:id=\"tecnachianfxid\" was not injected: check your FXML file 'ContentManagerApplyforLoanView.fxml'.";
+
+    }
+
+}
