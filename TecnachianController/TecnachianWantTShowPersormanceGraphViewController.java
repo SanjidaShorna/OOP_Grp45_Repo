ca09adf870c88f18227bb7612973cf15bd6e455e
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 20:59:41 +0600

    add

---
+++ TecnachianController/TecnachianWantTShowPersormanceGraphViewController.java
@@ -0,0 +1,20 @@
+package com.oop.mahadi.akash__dth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class TecnachianWantTShowPersormanceGraphViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
