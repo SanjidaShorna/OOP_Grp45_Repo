544599579a66a017768c7158b3ff90c9c20b7f43
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-18 11:50:09 +0600

    add

---
+++ ContentManagerControllerClass/ContentManagerViewshipFeedbacksceenController.java
@@ -0,0 +1,42 @@
+package com.oop.mahadi.demo6midexam;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ContentManagerViewshipFeedbacksceenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AddFeedbackButton;
+
+    @FXML
+    private ComboBox<?> ChannelComfxid;
+
+    @FXML
+    private TextField Viewcountfxid;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField contentId;
+
+    @FXML
+    private TextArea feedbacktext;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
