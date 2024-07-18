b9f73d836fa5e1bd661e361a21739d9c8e69dc61
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-18 11:38:56 +0600

    add

---
+++ ContentManagerControllerClass/ContentManagerSeeApplicationStatusController.java
@@ -0,0 +1,38 @@
+package com.oop.mahadi.demo6midexam;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ContentManagerSeeApplicationStatusController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> secCombox_Sub_App_iD;
+
+    @FXML
+    private TextArea showApplicationfxmltextarea;
+
+    @FXML
+    private TextField statusFxid;
+
+    @FXML
+    void backButon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
