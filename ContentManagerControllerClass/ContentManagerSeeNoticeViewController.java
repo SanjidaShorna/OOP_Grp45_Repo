61fe7c8c4bcee2eb2041f9b9cf4e9d138052f046
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-18 12:03:35 +0600

    Add done

---
+++ ContentManagerControllerClass/ContentManagerSeeNoticeViewController.java
@@ -0,0 +1,41 @@
+package com.oop.mahadi.demo6midexam;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ContentManagerSeeNoticeViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> comSunjectShow;
+
+    @FXML
+    private TextArea messagefxid;
+
+    @FXML
+    private TextField receiverid;
+
+    @FXML
+    private TextField senderId;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
