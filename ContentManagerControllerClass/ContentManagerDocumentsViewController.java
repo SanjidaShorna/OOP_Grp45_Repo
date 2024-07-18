dc238649c507b147b8cf908ad240d419ca8ce3fe
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-18 11:55:53 +0600

    add

---
+++ ContentManagerControllerClass/ContentManagerDocumentsViewController.java
@@ -0,0 +1,46 @@
+package com.oop.mahadi.demo6midexam;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ContentManagerDocumentsViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void Doc1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void d3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void d4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doc2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
